c628084538fdf192966dce0515c86cc0b8ff5d79
alantorresuam <125683087+alantorresuam@users.noreply.github.com>
2023-11-21 09:18:37 -0600

    este es un archivo nuevo

---
+++ holaMundo.py
@@ -0,0 +1,8 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre}!")
+    print(f"Tu número de matrícula es: {matricula}")
+
+if __name__ == "__main__":
+    nombre="alan torres"
+    matricula="2223028388"
+    saludar(nombre, matricula)
